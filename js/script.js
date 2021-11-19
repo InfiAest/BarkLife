77c4a859bcfe0cf1617afe2f62126195275a0d3d
Charlotte <charlotte.lucas@hotmail.com>
2021-11-19 16:43:46 +0100

    Homepage finished

--- js/script.js
+++ js/script.js
@@ -54,6 +54,9 @@ async function getFeaturedProducts() {
                                                     <h3>${products[i].name}</h3>
                                                     <p>£${products[i].price}</p>
                                                 </div>
+                                                <div class="cta-button-container">
+                                                    <a href="#" class="cta-button"><span>View product</span></a>
+                                                </div>
                                             </div>`
             }
 
@@ -65,3 +68,54 @@ async function getFeaturedProducts() {
 }
 
 getFeaturedProducts();
+
+
+//social media attests
+
+const socialMediaUrl = "https://charlotte-lucas-semproj2-api.herokuapp.com/social-media-attests";
+const attest1 = document.querySelector(".attest1");
+const attest2 = document.querySelector(".attest2");
+const attest3 = document.querySelector(".attest3");
+
+async function getSocialMediaAttests() {
+    try {
+        const response = await fetch(socialMediaUrl);
+        const instaMentions = await response.json();
+
+        for (i = 0; i < instaMentions.length; i++) {
+            console.log(instaMentions[i]);
+
+            attest1.innerHTML = `<div class="attest">
+                                                <div class="insta-img-container">
+                                                    <div class="insta-img-lrg" style="background-image: url('${instaMentions[0].insta_image.url}');">
+                                                </div>
+                                                <div class="insta-info-container">
+                                                    <p class="insta-username">${instaMentions[0].username} - ${instaMentions[0].comment}</p>
+                                                </div>
+                                            </div>`;
+        
+            attest2.innerHTML = `<div class="attest">
+                                            <div class="insta-img-container">
+                                                <div class="insta-img-sm" style="background-image: url('${instaMentions[1].insta_image.url}');">
+                                            </div>
+                                            <div class="insta-info-container">
+                                                <p class="insta-username">${instaMentions[1].username}</p>
+                                            </div>
+                                        </div>`;
+
+            attest3.innerHTML = `<div class="attest">
+                                                <div class="insta-img-container">
+                                                    <div class="insta-img-sm" style="background-image: url('${instaMentions[2].insta_image.url}');">
+                                                </div>
+                                                <div class="insta-info-container">
+                                                    <p class="insta-username">${instaMentions[2].username}</p>
+                                                </div>
+                                            </div>`;
+        }
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
+
+getSocialMediaAttests();
